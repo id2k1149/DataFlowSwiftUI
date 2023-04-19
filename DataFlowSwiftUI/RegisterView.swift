d11d5b2f17c0cb228bbfca614bbc71d4fad8bd9c
id2k1149 <id2k1149@gmail.com>
2023-04-19 11:29:52 -0400

    add struct RegisterView

---
+++ DataFlowSwiftUI/RegisterView.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterView.swift
+//  DataFlowSwiftUI
+//
+//  Created by Max Franz Immelmann on 4/19/23.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @State private var name = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Enter your name", text: $name)
+                .multilineTextAlignment(.center)
+            Button(action: registerUser) {
+                HStack {
+                    Image(systemName: "checkmark.circle")
+                    Text("OK")
+                }
+            }
+        }
+    }
+    
+    private func registerUser() {
+        
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
