45f3c0acd64e2a174395945b12e909cbf5f1b529
id2k1149 <id2k1149@gmail.com>
2023-04-19 09:28:54 -0400

    Initial Commit

---
+++ DataFlowSwiftUI/DataFlowSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowSwiftUIApp.swift
+//  DataFlowSwiftUI
+//
+//  Created by Max Franz Immelmann on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
