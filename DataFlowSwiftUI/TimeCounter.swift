4e239b22339009ab89523db73e7ac12ff6516fa1
id2k1149 <id2k1149@gmail.com>
2023-04-19 10:51:47 -0400

    add class TimeCounter

---
+++ DataFlowSwiftUI/TimeCounter.swift
@@ -0,0 +1,35 @@
+//
+//  TimeCounter.swift
+//  DataFlowSwiftUI
+//
+//  Created by Max Franz Immelmann on 4/19/23.
+//
+
+import Foundation
+
+class TimeCounter {
+    var counter = 3
+    var timer: Timer?
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(updateCounter),
+            userInfo: nil,
+            repeats: true)
+    }
+    
+    @objc private func updateCounter() {
+        if counter > 0 {
+            counter -= 1
+        } else {
+            killTimer()
+        }
+    }
+    
+    private func killTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
